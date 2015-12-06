080b6bf404e7d957c12b3e28e808508b325a3619
Edgar Montano <em2532@nyu.edu>
2015-12-06 00:41:06 -0500

    added docstring to commonlib

--- commonlib.py
+++ commonlib.py
@@ -3,6 +3,13 @@ import os
 
 
 def splitFile(fd,block_size):
+    """Takes a string of a file and splits file into array based on
+    block size.
+
+    fd - string of desired file to split
+    Block_size - desire block size to split file
+    """
+
     #new_block is a new array that contains the 
     #contents of the file, split based on block_size
     new_block = []
@@ -13,6 +20,7 @@ def splitFile(fd,block_size):
                 new_block.append(block)
             else:
                 break
-        return new_block
+        
+    return new_block
 
         
